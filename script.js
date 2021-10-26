b59a334cb9e58a4f36dafa65a9042d12c3eb6690
GEORGE CORREIA LEAL NETO <21193368@CFP127ALUNO.LOCAL>
2021-10-26 14:19:54 -0300

    Em andamento...

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 'use strict';
 const pesquisarCep =  async (cep) => {
-    fetch(url);
-    return `CEP Digitado ${cep}`; 
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
 
-const preencherFormulario = () => {
-    const cep = document.querySelector('#cep')
-    const infoCep = pesquisarCep(cep);
+const preencherFormulario = async (evento) => {
+    const cep = evento.target.value;
+
+    const infoCep = await pesquisarCep(cep);
+
+    document.querySelector('#endereco').value = infoCep.logradouro;
+    document.querySelector('#numero').value = infoCep.complemento;
+    document.querySelector('#bairro').value = infoCep.bairro;
+    document.querySelector('#cidade').value = infoCep.localidade;
+    document.querySelector('#estado').value = infoCep.uf;
 
     console.log(infoCep);
 }
